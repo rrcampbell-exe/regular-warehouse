da7227ea681e8915206de038faad049d947badf9
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-16 07:00:46 -0500

    Create typeDefs for User model

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+// import gql function (tagged template)
+const { gql } = require("apollo-server-express");
+
+// create typeDefs
+const typeDefs = gql`
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+`;
+
+// export typeDefs
+module.exports = typeDefs;
