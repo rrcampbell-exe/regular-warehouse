6e4fb68a52477b60818c6691adee95b841a90f71
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-21 09:19:31 -0500

    Create Footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>Nothing extraordinary will happen if you click here.</span>
+    </footer>
+  );
+};
+
+export default Footer;
