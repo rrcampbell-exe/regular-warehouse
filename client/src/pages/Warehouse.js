c9a6f6e5889d5df5d1e08397bcdec398e44c0073
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-21 07:05:15 -0500

    Create Warehouse page component

---
+++ client/src/pages/Warehouse.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_ALL_ITEMS } from "../utils/queries";
+import ItemList from "../components/ItemList";
+
+const Warehouse = () => {
+  const { loading, data } = useQuery(QUERY_ALL_ITEMS);
+
+  const items = data?.items || [];
+  console.log(items)
+
+  return (
+    <main>
+      <div>
+        {loading ? (
+          <div>Pawing about the warehouse in search of items...</div>
+        ) : (
+          <ItemList items={items} />
+        )}
+      </div>
+    </main>
+  );
+
+}
+
+export default Warehouse;
