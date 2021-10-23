37eb5bc91403beaef258aa66d514154cce102742
Kait <kweishaar@hawk.iit.edu>
2021-10-23 09:17:21 -0500

    added homepage, probably doesn't work yet but hey getting those commits in

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home  = () => {
+    return(
+        <div>
+            <h1>Regular Warehouse</h1>
+            <p>Where nothing irreguular ever happens</p>
+            <a href="/login">Log In</a>
+            <a href="/signup">Sign Up</a>
+        </div>
+    )
+}
+
+
+export default Home;
