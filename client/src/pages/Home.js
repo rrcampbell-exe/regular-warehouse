f78d3bc42abfe1cf8a4121f31e8eecf6abe8797e
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-24 06:45:54 -0500

    Create home page component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+
+  return (
+    <main>
+      <h1>Regular Warehouse.</h1>
+      <p>You can only enter this totally normal warehouse if you know the password. Your password.</p>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+      <Link to="/signup">
+        <button>Signup</button>
+      </Link>
+    </main>
+  )
+}
+
+export default Home;
