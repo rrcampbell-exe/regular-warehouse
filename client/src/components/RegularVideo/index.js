12613d1e1c3bf6447a0872aaba3d584d14bb8a5b
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-24 07:19:27 -0500

    Update classNames

--- client/src/components/RegularVideo/index.js
+++ client/src/components/RegularVideo/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RegularVideo = () => {
   return (
-    <video id="regular-video" controls>
+    <video id="regular-video" className="regular-video-invisible">
       <source src="./video/irregular-warehouse.mp4"></source>
     </video>
   )
