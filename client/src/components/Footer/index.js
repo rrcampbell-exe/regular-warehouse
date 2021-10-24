52422268185c170ab19d68650e33e36dd610287e
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-24 07:19:07 -0500

    Update to play video on click

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
+  const regularVideo = document.getElementById("regular-video");
 
+  function removeVideo() {
+    regularVideo.classList.remove("regular-video-visible");
+    regularVideo.classList.add("regular-video-invisible");
+  }
+  
   function playVideo() {
-
+    regularVideo.classList.remove("regular-video-invisible");
+    regularVideo.classList.add("regular-video-visible");
+    regularVideo.requestFullscreen()
+    regularVideo.play()
+    setTimeout(removeVideo, 66000)
   }
 
   return (
     <footer>
-      <span onClick={playVideo}>Nothing extraordinary will happen if you click here.</span>
+      <span onClick={playVideo}>
+        Nothing extraordinary will happen if you click here.
+      </span>
     </footer>
   );
 };
