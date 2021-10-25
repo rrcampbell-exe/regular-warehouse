717ef7d3a0db05aba00df0cddf79d0f9d6c36dc1
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-25 16:48:53 -0500

    Minor style changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
 
   return (
     <main>
-      <h1>Regular Warehouse.</h1>
+      <h1>📦 Regular Warehouse.</h1>
       <p>You can only enter this totally normal warehouse if you know the password. Your password.</p>
       <Link to="/login">
         <button>Login</button>
