cea9bdc08a00dacde7f3a39419d1f661a9b6dfee
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-26 15:34:19 -0500

    Minor style updates

--- client/src/components/NewItem/index.js
+++ client/src/components/NewItem/index.js
@@ -1,102 +1,96 @@
-import { useMutation } from '@apollo/client';
-import React, { useState } from 'react';
-import  { ADD_ITEM } from '../../utils/mutations';
-import { QUERY_ALL_ITEMS } from '../../utils/queries';
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
+import { ADD_ITEM } from "../../utils/mutations";
+import { QUERY_ALL_ITEMS } from "../../utils/queries";
 
-const faker = require('faker/locale/en_US');
-
-
+const faker = require("faker/locale/en_US");
 
 function NewItem() {
-    const [formData, setFormData] = useState({ name: '', quantity: '' });
-    const [addItem, { error }] = useMutation(ADD_ITEM, {
-        update(cache, { data: { addItem } }) {
-            try {
-                const { items } = cache.readQuery({ query: QUERY_ALL_ITEMS });
+  const [formData, setFormData] = useState({ name: "", quantity: "" });
+  const [addItem, { error }] = useMutation(ADD_ITEM, {
+    update(cache, { data: { addItem } }) {
+      try {
+        const { items } = cache.readQuery({ query: QUERY_ALL_ITEMS });
 
-                cache.writeQuery({
-                    query: QUERY_ALL_ITEMS,
-                    data: { items: [addItem, ...items] }
-                });
-            } catch (e) {
-                console.error(e);
-            }
+        cache.writeQuery({
+          query: QUERY_ALL_ITEMS,
+          data: { items: [addItem, ...items] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  });
 
-        }
+  const handleChange = (event) => {
+    // Add an if statement to limit the length if necessary. If so, add characterCount state
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // create part number with faker
+    const pnProto = faker.datatype.number({
+      min: 100000,
+      max: 199999,
     });
 
-    const handleChange = event => {
-        // Add an if statement to limit the length if necessary. If so, add characterCount state
-        const { name, value } = event.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
+    let pnProtoArr = pnProto.toString().split("");
+    pnProtoArr.splice(3, 0, "-");
+    let part_number = pnProtoArr.join("");
+    // console.log(part_number);
+    // console.log(formData);
+    // console.log(parseInt(formData.quantity));
 
-    const handleFormSubmit = async event => {
-        event.preventDefault();
+    try {
+      // add item to database
+      await addItem({
+        variables: {
+          name: formData.name,
+          part_number: part_number,
+          quantity: parseInt(formData.quantity),
+        },
+      });
 
-        // create part number with faker
-        const pnProto = faker.datatype.number({
-            'min': 100000,
-            'max': 199999
-          });
-      
-        let pnProtoArr = pnProto.toString().split("")
-        pnProtoArr.splice(3, 0, "-")
-        let part_number = pnProtoArr.join("")
-        // console.log(part_number);
-        // console.log(formData);
-        // console.log(parseInt(formData.quantity));
+      // Double check that this works (i'm suspicious)
+      setFormData({ name: "", quantity: "" });
+      // console.log(formData);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-        try {
-            // add item to database
-            await addItem({
-                variables: {
-                    name: formData.name,
-                    part_number: part_number,
-                    quantity: parseInt(formData.quantity),
-                }
-            });
+  return (
+    <div>
+      <p>{error && <span>Something irregular has occurred... 🤔</span>}</p>
+      <h2>Create New Item</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="name">Item Name:</label>
+        <input
+          placeholder="Item name"
+          name="name"
+          type="text"
+          value={formData.name} // -- is this necessary?
+          onChange={handleChange}
+        />
+        <label htmlFor="quantity">Item Quantity:</label>
+        <input
+          placeholder="Item quantity"
+          name="quantity"
+          type="text"
+          value={formData.quantity}
+          onChange={handleChange} // -- is this necessary?
+        />
 
-            // Double check that this works (i'm suspicious)
-            setFormData({ name: '', quantity: '' });
-            // console.log(formData);
-        } catch (e) {
-            console.error(e);
-        }
-    };
-
-    return (
-        <div>
-            <p>
-                {error && <span>Something irregular has occurred... 🤔</span>}
-            </p>
-            <h2>Add New Item</h2>
-            <form onSubmit = {handleFormSubmit}>
-                <label htmlFor = "name">Item Name:</label>
-                <input 
-                    placeholder = "Item name" 
-                    name = "name" 
-                    type = "text"
-                    value = {formData.name}  // -- is this necessary?
-                    onChange = {handleChange}   
-                />
-                <label htmlFor = "quantity">Item Quantity:</label>
-                <input
-                    placeholder = "Item quantity"
-                    name = "quantity"
-                    type = "text"
-                    value = {formData.quantity}
-                    onChange = {handleChange}  // -- is this necessary?
-                />
-
-                <button type = "submit">Submit Item</button>
-            </form>
-
-        </div>
-    );
+        <button type="submit">Create Item</button>
+      </form>
+    </div>
+  );
 }
 
 export default NewItem;
