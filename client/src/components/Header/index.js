b6ce2767b0fccb99983b65e656df3b4315529a2a
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-25 19:20:54 -0500

    Add logout functionality

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-
+import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    
-      return (
-        <header>
-            <h1>📦 Regular Warehouse.</h1>
-            <Link to="/">Log Out</Link>
-        </header>
-      );
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  return (
+    <header>
+      <h1>📦 Regular Warehouse.</h1>
+      <Link to="/" onClick={logout}>Log Out</Link>
+    </header>
+  );
 };
 
 export default Header;
