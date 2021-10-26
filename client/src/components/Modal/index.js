87b32ef9dac3c87522fda3ba241c61c4b6518dfd
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-25 20:15:08 -0500

    Begin modal build

---
+++ client/src/components/Modal/index.js
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from 'react';
+
+const Modal = () => {
+
+  const regularVideo = document.getElementById("regular-video");
+
+  const modalEl = useRef(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      modalEl.current.classList.add("modal-container-visible");
+      modalEl.current.classList.remove("modal-container-invisible")
+    }, 5000)
+    return () => {}
+  });
+
+  function removeVideo() {
+    regularVideo.classList.remove("regular-video-visible");
+    regularVideo.classList.add("regular-video-invisible");
+  }
+  
+  function playVideo() {
+    regularVideo.classList.remove("regular-video-invisible");
+    regularVideo.classList.add("regular-video-visible");
+    regularVideo.requestFullscreen()
+    regularVideo.play()
+    setTimeout(removeVideo, 66000)
+  }
+
+  function closeModal() {
+    modalEl.classList.remove("modal-container-visible");
+    modalEl.classList.add("modal-container-invisible");
+  }
+
+  return (
+    <div ref={modalEl} className="modal-container-invisible" id="modal">
+      <h3>Is the coast clear? 👀</h3>
+      <button onClick={playVideo}>🕺🐈💃</button>
+      <button onClick={closeModal}>😓😬😨</button>
+    </div>
+  )
+}
+
+export default Modal;
