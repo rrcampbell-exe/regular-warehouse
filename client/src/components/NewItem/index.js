8862d761eeaffd0148b873e68569dff208b74b95
Dylan K <drkunkel7@gmail.com>
2021-10-27 10:36:16 -0500

    custom part numbers/auto part numbers - YOU CHOOSE

--- client/src/components/NewItem/index.js
+++ client/src/components/NewItem/index.js
@@ -5,10 +5,10 @@ import { QUERY_ALL_ITEMS } from '../../utils/queries';
 
 const faker = require('faker/locale/en_US');
 
-
+let newPart_number // Setting this variable in a larger scope, so mutliple functions can access it
 
 function NewItem() {
-    const [formData, setFormData] = useState({ name: '', quantity: '' });
+    const [formData, setFormData] = useState({ name: '', part_number: '', quantity: '' });
     const [addItem, { error }] = useMutation(ADD_ITEM, {
         update(cache, { data: { addItem } }) {
             try {
@@ -36,32 +36,35 @@ function NewItem() {
 
     const handleFormSubmit = async event => {
         event.preventDefault();
-
-        // create part number with faker
-        const pnProto = faker.datatype.number({
-            'min': 100000,
-            'max': 199999
-          });
-      
-        let pnProtoArr = pnProto.toString().split("")
-        pnProtoArr.splice(3, 0, "-")
-        let part_number = pnProtoArr.join("")
-        // console.log(part_number);
-        // console.log(formData);
-        // console.log(parseInt(formData.quantity));
-
+        // console.log(formData.part_number)
+        if (!formData.part_number) {
+            // create part number with faker
+            const pnProto = faker.datatype.number({
+                'min': 100000,
+                'max': 199999
+            });
+        
+            let pnProtoArr = pnProto.toString().split("")
+            pnProtoArr.splice(3, 0, "-")
+            newPart_number = pnProtoArr.join("")
+            // console.log(newPart_number)
+            // console.log(formData)
+        } else {
+            newPart_number = formData.part_number
+        }
+        
         try {
             // add item to database
             await addItem({
                 variables: {
                     name: formData.name,
-                    part_number: part_number,
+                    part_number: newPart_number,
                     quantity: parseInt(formData.quantity),
                 }
             });
 
             // Double check that this works (i'm suspicious)
-            setFormData({ name: '', quantity: '' });
+            setFormData({ name: '', part_number: '', quantity: '' });
             // console.log(formData);
         } catch (e) {
             console.error(e);
@@ -91,6 +94,15 @@ function NewItem() {
                     value = {formData.quantity}
                     onChange = {handleChange}  // -- is this necessary?
                 />
+                <label htmlFor = "part_number">Part Number:</label>
+                <input
+                    placeholder = "Part number"
+                    name = "part_number"
+                    type = "text"
+                    value = {formData.part_number}
+                    onChange = {handleChange}
+                />
+                <p>A new part number will be created if this space is left blank.</p>
 
                 <button type = "submit">Submit Item</button>
             </form>
