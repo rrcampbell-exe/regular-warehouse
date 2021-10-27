84b106d6c17dda783428845101346ffe152ae4bf
Dylan K <drkunkel7@gmail.com>
2021-10-27 11:41:58 -0500

    attempted to compare part numbers

--- client/src/components/NewItem/index.js
+++ client/src/components/NewItem/index.js
@@ -1,4 +1,4 @@
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import React, { useState } from 'react';
 import  { ADD_ITEM } from '../../utils/mutations';
 import { QUERY_ALL_ITEMS } from '../../utils/queries';
@@ -7,13 +7,15 @@ const faker = require('faker/locale/en_US');
 
 let newPart_number // Setting this variable in a larger scope, so mutliple functions can access it
 
+
+
 function NewItem() {
     const [formData, setFormData] = useState({ name: '', part_number: '', quantity: '' });
     const [addItem, { error }] = useMutation(ADD_ITEM, {
         update(cache, { data: { addItem } }) {
             try {
                 const { items } = cache.readQuery({ query: QUERY_ALL_ITEMS });
-
+                console.log(items[0].part_number)
                 cache.writeQuery({
                     query: QUERY_ALL_ITEMS,
                     data: { items: [addItem, ...items] }
@@ -24,19 +26,9 @@ function NewItem() {
 
         }
     });
-
-    const handleChange = event => {
-        // Add an if statement to limit the length if necessary. If so, add characterCount state
-        const { name, value } = event.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    const handleFormSubmit = async event => {
-        event.preventDefault();
-        // console.log(formData.part_number)
+    // const { data } = useQuery(QUERY_ALL_ITEMS); // To use in the generatetPartNumber function
+    // console.log(data.items)
+    function generatePartNumber() {
         if (!formData.part_number) {
             // create part number with faker
             const pnProto = faker.datatype.number({
@@ -47,12 +39,38 @@ function NewItem() {
             let pnProtoArr = pnProto.toString().split("")
             pnProtoArr.splice(3, 0, "-")
             newPart_number = pnProtoArr.join("")
-            // console.log(newPart_number)
-            // console.log(formData)
+            
+            // console.log(data.items)
+            // for (let i = 0; i < data.items.length; i++) {  // Compare newPart_number with all others
+            //     if (data.items[i].part_number === newPart_number) {  // If there's a match, rerun function to generate new number
+            //         return generatePartNumber();
+            //     }
+            // }
         } else {
+            // for (let i = 0; i < data.items.length; i++) {  // Compare newPart_number with all others
+            //     if (data.items[i].part_number === newPart_number) {  // If there's a match, tell the user
+            //         console.log("This part number is already in use")
+            //         return; // Just return?
+            //     }
+            // }
             newPart_number = formData.part_number
         }
+    }
+
+    const handleChange = event => {
+        // Add an if statement to limit the length if necessary. If so, add characterCount state
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+        // console.log(formData.part_number)
         
+        generatePartNumber();
+
         try {
             // add item to database
             await addItem({
