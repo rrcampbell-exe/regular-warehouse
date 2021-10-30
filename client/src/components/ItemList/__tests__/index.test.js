380a0f5b8e0150310f69734ddad08b668ab3c23e
Dylan K <drkunkel7@gmail.com>
2021-10-30 13:31:43 -0500

    attempted to wrap components in ApolloProvider

--- client/src/components/ItemList/__tests__/index.test.js
+++ client/src/components/ItemList/__tests__/index.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ItemList from '..';
+import { ApolloProvider } from '@apollo/client';
 
 afterEach(cleanup);
 
 describe('ItemList component', () => {
     it('renders', () => { // this does not pass
-        render(<ItemList/>)
+        render(<ApolloProvider><ItemList/></ApolloProvider>)
     })
 })
--- client/src/components/NewItem/__tests__/index.test.js
+++ client/src/components/NewItem/__tests__/index.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import NewItem from '..';
+import { ApolloProvider } from '@apollo/client';
 
 afterEach(cleanup);
 
