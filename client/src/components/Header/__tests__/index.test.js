523c3ed5d37fe44141cf01486c6887b3e23e3c92
Dylan K <drkunkel7@gmail.com>
2021-10-30 13:36:55 -0500

    fixed <Link> outisde of <Router> error

--- client/src/components/Header/__tests__/index.test.js
+++ client/src/components/Header/__tests__/index.test.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Header from '..';
+import { BrowserRouter } from 'react-router-dom';
 
 afterEach(cleanup);
 
 describe('Header component', () => {
-    it('renders', () => { // this does not pass
-        render(<Header/>)
+    it('renders', () => { // this does not pass (I think BrowserRouter fixed it?)
+        render(<BrowserRouter><Header/></BrowserRouter>)
     })
     it('matches snapshot', () => {
         const { asFragment } = render(<Header/>); // does not pass
