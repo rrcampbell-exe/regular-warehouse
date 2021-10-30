c61d7c9e1446ce55bfe1704c1203e4c31ec479ae
Kait <kweishaar@hawk.iit.edu>
2021-10-30 15:48:54 -0500

    shadows

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -9,7 +9,7 @@ const Header = () => {
   };
 
   return (
-    <header className='bg-light border shadow-sm mb-3 p-1'>
+    <header className='bg-light border shadow-sm mb-3 p-1 text-shadow'>
       <h1>📦 Regular Warehouse.</h1>
       <Link to="/" onClick={logout} className='nav nav-link'><span className="material-icons md-12 align-bottom">logout</span>Log Out</Link>
     </header>
