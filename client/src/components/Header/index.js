e4456edc8e0e227772c9ad4e00ecb63ddcd3c130
Olga <o.smolyakova30@gmail.com>
2021-10-31 12:50:29 -0500

    link back to warehouse page from edit item

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,7 +10,9 @@ const Header = () => {
 
   return (
     <header className='bg-light border shadow-sm p-1 text-shadow'>
-      <h1 className="align-bottom">📦 Regular Warehouse.</h1>
+      <Link to='/warehouse' className='nav nav-link text-dark' >
+        <h1 className="align-bottom">📦 Regular Warehouse.</h1>
+        </Link>
       <Link to="/" onClick={logout} className='nav nav-link align-top text-dark p-0'><span className="material-icons md-12 align-bottom">logout</span>Log Out</Link>
     </header>
   );
